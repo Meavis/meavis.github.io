8bfc34422f66b5b3a74d715019879479e447655e
Allysha Sewradj <allysha99@hotmail.com>
2017-09-21 08:54:08 +0200

    functies aangepas en close nav toegevoegd (werkt nog niet goed)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-openNav{
+
+/* function openNav {
   document.getElementById("mySidenav").style.width = "250px";
   document.body.style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}*/
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
 }
