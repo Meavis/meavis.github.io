6e8dda85c0876857bd6cafcf6f0c1662592cacc1
Allysha Sewradj <allysha99@hotmail.com>
2017-09-21 09:41:06 +0200

    nietwerkende code weggehaald

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-/* function openNav {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
-}*/
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.marginLeft = "250px";
