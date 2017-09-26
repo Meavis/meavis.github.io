dd5ae40819a72bfb6beb34cc5035f5340e24c141
Allysha Sewradj <allysha99@hotmail.com>
2017-09-26 09:37:46 +0200

    er zat een s achter element voor main

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.marginLeft = "250px";
-    document.getElementsByClassName("main").style.opacity = "0.5";
+    document.getElementById("main").style.opacity = "0.5";
     //document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.marginLeft= "0";
-    document.getElementsByClassName("main").style.opacity = "1";
+    document.getElementById("main").style.opacity = "1";
     //document.body.style.backgroundColor = "#8d99ae";
 }
