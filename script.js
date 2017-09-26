7911add4e0e097c1d9079bd672f921feb75cbc75
Allysha Sewradj <allysha99@hotmail.com>
2017-09-26 09:50:30 +0200

    fixMenubtn opacity ook veranderd

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.marginLeft = "250px";
     document.getElementById("main").style.opacity = "0.5";
+    document.getElementById("fixMenubtn").style.opacity = "0.5";
     //document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
 }
 
@@ -9,5 +10,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.marginLeft= "0";
     document.getElementById("main").style.opacity = "1";
+    document.getElementById("fixMenubtn").style.opacity = "1";
     //document.body.style.backgroundColor = "#8d99ae";
 }
