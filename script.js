b855a9f0a6b7a5fc8ea62de368ab67fdf6b2aa14
Allysha Sewradj <allysha99@hotmail.com>
2017-10-05 10:03:54 +0200

    uitleg toegevoegd

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
+    //onderstaande code meeschuiven van inhoud
     //document.body.style.marginLeft = "250px";
     document.getElementById("main").style.opacity = "0.5";
     document.getElementById("fixMenubtn").style.opacity = "0.5";
-    //document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
+    //onderstaande code terugschuiven van inhoud
     //document.body.style.marginLeft= "0";
     document.getElementById("main").style.opacity = "1";
     document.getElementById("fixMenubtn").style.opacity = "1";
