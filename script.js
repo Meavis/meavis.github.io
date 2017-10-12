e6737bd833cbd61779d2cb6c6221bc5a61341c93
Allysha Sewradj <allysha99@hotmail.com>
2017-10-12 09:12:10 +0200

    transition en schuiven uit comments gehaald

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     //onderstaande code meeschuiven van inhoud
-    //document.body.style.marginLeft = "250px";
+    document.body.style.marginLeft = "250px";
     document.getElementById("main").style.opacity = "0.5";
     document.getElementById("fixMenubtn").style.opacity = "0.5";
 }
@@ -9,7 +9,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
     //onderstaande code terugschuiven van inhoud
-    //document.body.style.marginLeft= "0";
+    document.body.style.marginLeft= "0";
     document.getElementById("main").style.opacity = "1";
     document.getElementById("fixMenubtn").style.opacity = "1";
 }
